a3576cce37b400eb2dd465d1b3b7bc2541e0ab63
Haifeng Wang <wang19891218@gmail.com>
2020-10-23 02:01:29 -0400

    Update scihub_automatic_popup.js

--- scihub_automatic_popup.js
+++ scihub_automatic_popup.js
@@ -1,12 +1,17 @@
-
 function addElement () { 
   // create a new div element
   const newDiv = document.createElement("div"); 
   
   // and give it some content
   const newIframe = document.createElement('iframe'); 
-  newIframe.src = 'sci-hub.st' + window.location.href
+  newIframe.style.width = window.innnerWidth // 2;
+  newIframe.style.height = window.innerHeight // 2;
+  newIframe.style.position = "absolut";
+  newIframe.style.top = "10px";
+  newIframe.src = 'sci-hub.st/' + window.location.href
   // add the text node to the newly created div
-  newDiv.appendChild(newIframe);  
-
-  doc
+  newDiv.appendChild(newIframe);
+  document.body.appendChild(newDiv);
+  console.log(newIframe.src)
+  console.log(newIframe)
+}
