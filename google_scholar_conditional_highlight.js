a10b768e2385f5cba021b288ff1f1eefc35b0a1b
Haifeng Wang <wang19891218@gmail.com>
2021-02-23 03:46:03 -0500

    Update google_scholar_conditional_highlight.js

--- google_scholar_conditional_highlight.js
+++ google_scholar_conditional_highlight.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         Google Scholar Highlight
+// @name         Google scholar conditional highlight
 // @namespace    wang19891218
-// @version      0.1
+// @version      0.2
 // @description  Save some time
 // @author       coco
 // @match        https://scholar.google.com/scholar*
 // @grant        none
 // ==/UserScript==
-
+ 
 function Function_High_Light(){
     var List_Class = document.getElementsByClassName("gs_fl");
     var var_h_coord = window.innerWidth - 220
@@ -30,8 +30,8 @@ function Function_High_Light(){
     document.cookie = "Input_Cite_Value=" + div_Input_Cite.value
     return true
 }
-
-
+ 
+ 
 function Function_Highlight_Year() {
     var Array_gs_a = document.getElementsByClassName('gs_a')
     var Int_Threshold_Year = div_Input_Year.value
@@ -52,14 +52,14 @@ function Function_Highlight_Year() {
     document.cookie = "Input_Year_Threshold=" + div_Input_Year.value
     return true
 }
-
+ 
 var Str_search_Text = "Cited";
 var List_Found = [];
 var List_a_Tag = [];
 var Str_Citation_Number;
 var Value_Citation_Number;
-
-
+ 
+ 
 var div_Control = document.createElement("div");
 var var_v_coord = 180
 var var_h_coord = window.innerWidth - 220
@@ -73,9 +73,9 @@ div_Control.style.background = "gray";
 div_Control.style.color = "white";
 div_Control.id = "id_coco_scholar_control"
 // div_Control.innerHTML = "Control";
-
+ 
 var decodedCookie = decodeURIComponent(document.cookie);
-
+ 
 function getCookie(name) {
   var decodedCookie = decodeURIComponent(document.cookie);
   var ca = decodedCookie.split(';');
@@ -90,15 +90,15 @@ function getCookie(name) {
   }
   return "0";
 }
-
-
+ 
+ 
 var div_Input_Cite_Title = document.createElement("div");
 div_Input_Cite_Title.innerText="Citation threshold"
 div_Input_Cite_Title.class="column"
 div_Input_Cite_Title.style.width = "200px"
 div_Input_Cite_Title.style.height = "20px"
 div_Input_Cite_Title.style.float = "left"
-
+ 
 var div_Input_Cite = document.createElement("INPUT");
 div_Input_Cite.setAttribute("type", "text");
 div_Input_Cite.name= "name"
@@ -109,14 +109,14 @@ div_Input_Cite.value=parseInt(getCookie("Input_Cite_Value"));
 if (isNaN(div_Input_Cite.value)) div_Input_Cite.value = 2000;
 document.cookie = "Input_Cite_Value=" + div_Input_Cite.value
 div_Input_Cite.onchange = function(){Function_High_Light()}
-
+ 
 var div_Input_Year_Title = document.createElement("div");
 div_Input_Year_Title.innerText="Year threshold"
 div_Input_Year_Title.class="column"
 div_Input_Year_Title.style.width = "200px"
 div_Input_Year_Title.style.height = "20px"
 div_Input_Year_Title.style.float = "left"
-
+ 
 var div_Input_Year = document.createElement("INPUT");
 div_Input_Year.setAttribute("type", "text");
 div_Input_Year.name= "name"
@@ -127,22 +127,22 @@ div_Input_Year.value=parseInt(getCookie("Input_Year_Threshold"));
 if (isNaN(div_Input_Year.value)) div_Input_Year.value = 2000;
 document.cookie = "Input_Year_Threshold=" + div_Input_Year.value
 div_Input_Year.onchange = function(){Function_Highlight_Year()}
-
+ 
 window.addEventListener("resize", Function_High_Light);
-
+ 
 div_Control.appendChild(div_Input_Cite_Title)
 div_Control.appendChild(div_Input_Cite)
 div_Control.appendChild(div_Input_Year_Title)
 div_Control.appendChild(div_Input_Year)
-
+ 
 document.getElementsByTagName("body")[0].appendChild(div_Control)
-
-
+ 
+ 
 Function_High_Light()
 Function_Highlight_Year()
-
-
-
+ 
+ 
+ 
 // Element property hidden
 // height: 0px;
 // visibility: hidden;
