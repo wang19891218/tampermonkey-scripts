911e2c07992870e814021f5c22df631ea9d5272a
wang19891218 <wang19891218@gmail.com>
2021-05-19 01:42:58 -0400

    updated sci-hub popup

--- google_scholar/google_scholar_sci_hub_popup.js
+++ google_scholar/google_scholar_sci_hub_popup.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Google scholar sci-hub popup 
 // @namespace    wang19891218
-// @version      0.45
+// @version      0.48
 // @description  Save some time with sci-hub
 // @author       coco
+// @require      https://code.jquery.com/jquery-3.6.0.slim.min.js
+
 // @include      https://ascelibrary.org/doi/pdf*
 // @include      https://aip.scitation.org/doi/abs/*
 // @include      https://journals.aps.org/pre/abstract/*
@@ -20,41 +22,32 @@
 // ==/UserScript==
 
 // Immediately-invoked function expression
-(function() {
-    // Load the script
-    var script = document.createElement("SCRIPT");
-    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
-    script.type = 'text/javascript';
-    script.onload = function() {
-        var $ = window.jQuery;
-        // Use $ here...
-    };
-    document.getElementsByTagName("head")[0].appendChild(script);
-})();
 
 function addSciHubElement () {
   // create a new div element
   const newDiv = document.createElement("div");
-
+  newDiv.setAttribute("id", "frame-for-sci-hub");
   // and give it some content
   const newIframe = document.createElement('iframe');
   console.log(window.innerWidth)
   console.log(window.innerWidth / 2)
   newDiv.style.zIndex= 20;
-  newDiv.style.width = "400px"
+  newDiv.style.width = "360px"
   newDiv.style.height = "600px"
-  newDiv.style.position = "absolute";
-  newDiv.style.bottom = ($(window).height() - 4).toString + 'px'
-  newDiv.style.right = '2px'
-  // newIframe.style
+  newDiv.style.position = "fixed"
+  newDiv.style.top = 0
+  newDiv.style.right = 0
+  newIframe.style
   newIframe.style.width= '100%'
   newIframe.style.height= '100%'
   newIframe.src = 'https://sci-hub.st/' + window.location.href
+  newIframe.style.overflow = "hidden"
   // add the text node to the newly created div
   newDiv.appendChild(newIframe);
   document.body.appendChild(newDiv);
-  console.log(newIframe.src)
-  console.log(newIframe)
+  // console.log(newIframe.src)
+  // console.log(newIframe)
+  $("#frame-for-sci-hub").draggable({ handle:'.header'})
 }
 
 addSciHubElement()
