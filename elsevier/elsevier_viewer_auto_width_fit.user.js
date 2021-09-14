90e0e02e76c577dfcefe29395ee5490d8ac63d9c
wang19891218 <wang19891218@gmail.com>
2021-09-14 10:50:31 -0400

    added elsvier pdf viewer

---
+++ elsevier/elsevier_viewer_auto_width_fit.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// // @name         elsvier pdf viewer auto width expand
+// // @namespace    http://tampermonkey.net/
+// // @version      0.1
+// // @description  try to take over the world!
+// // @author       wang19891218
+// // @match        https://reader.elsevier.com/reader/*
+// // @icon         https://www.google.com/s2/favicons?domain=elsevier.com
+// // @grant        none
+// // ==/UserScript==
+
+(function() {
+    'use strict';
+
+     // Your code here...
+    window.addEventListener('load', function_fit_to_width, false);
+})();
+
+
+function function_fit_to_width() {
+    var button = document.getElementById("fit-to-width-button")
+    button.click()
+}
