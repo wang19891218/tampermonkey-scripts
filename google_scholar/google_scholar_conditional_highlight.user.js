59a356c83eb4a35dbaa30f086a51c8654062a720
Haifeng Wang <wang19891218@gmail.com>
2021-10-13 10:52:33 -0400

    Update google_scholar_conditional_highlight.user.js

--- google_scholar/google_scholar_conditional_highlight.user.js
+++ google_scholar/google_scholar_conditional_highlight.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google scholar conditional highlight
 // @namespace    wang19891218
-// @version      0.40
+// @version      0.41
 // @description  Save some time
 // @author       coco
 // @updateURL    https://github.com/wang19891218/tampermonkey-scripts/raw/master/google_scholar/google_scholar_conditional_highlight.user.js
@@ -13,86 +13,65 @@
 // ==/UserScript==
 
 
-function FunctionHighlightCitation(){
-    // Highlight citation
-    var List_Class = document.getElementsByClassName("gs_fl");
-    var var_h_coord = window.innerWidth - 220
-    // div_Control.style.left = var_h_coord+"px"
-    var Int_Threshold_Value_Cite=div_Input_Cite.value
-    for (var i_Class = 0; i_Class < List_Class.length; i_Class++){
-        List_a_Tag = List_Class[i_Class].getElementsByTagName("a");
-        for (var i_a_Tag = 0; i_a_Tag < List_a_Tag.length; i_a_Tag++){
-            if (List_a_Tag[i_a_Tag].textContent.indexOf(Str_search_Text) == 0){
-                Str_Citation_Number = List_a_Tag[i_a_Tag].textContent
-                Value_Citation_Number = parseInt(Str_Citation_Number.substring(9, Str_Citation_Number.length))
-                if (Value_Citation_Number >= Int_Threshold_Value_Cite){
-                    List_a_Tag[2].style.backgroundColor = 'powderblue';
-                    List_a_Tag[2].style.border = "2px dashed black";
-                    // List_a_Tag[2].style.padding = "0.03em 0.05em";
-                    console.log(List_a_Tag[2].style.border);
-                } else {
-                    List_a_Tag[2].style.backgroundColor = '';
-                    List_a_Tag[2].style.border = "";
-                }
-            }
-        }
-    }
-    document.cookie = "Input_Cite_Value=" + div_Input_Cite.value
-    return true
-}
-
-function Function_Highlight_Year() {
-    // Highe light year
-    var Array_gs_a = document.getElementsByClassName('gs_a')
-    var Int_Threshold_Year = div_Input_Year.value
-    for (var i_gs=0; i_gs < Array_gs_a.length; i_gs++) {
-        var Div_gs_a = Array_gs_a[i_gs]
-        var Str_Year = Div_gs_a.textContent.match(/ [0-9][0-9][0-9][0-9] /g)[0]
-        // var Int_Start = Div_gs_a.textContent.lastIndexOf(',')
-        // var Int_Ended = Div_gs_a.textContent.lastIndexOf('-')
-        // var Str_Year = Div_gs_a.textContent.substring(Int_Start + 1,Int_Ended)
-        var Int_Year = parseInt(Str_Year)
-        var strStyle = "background-color:powderblue;display: inline-block; border: 2px dashed black; padding: 0.03em 0.05em;"
-        Div_gs_a.innerHTML = Div_gs_a.innerHTML.replace(strStyle,'');
-        if (Int_Year >= Int_Threshold_Year) {
-            // console.log(Div_gs_a.innerHTML);
-            Div_gs_a.innerHTML = Div_gs_a.innerHTML.replace(Str_Year,'<b style=\"' + strStyle + '\">' + Str_Year + '</b>');
-        } else {
-
-        }
-        // console.log(Str_Year)
-    }
-    document.cookie = "Input_Year_Threshold=" + div_Input_Year.value
-    return true
-}
-
-function FunctionAddAverageCitation() {
-    // Highe light year
+function FunctionModifyItems() {
+    // Modify items following current condition selection
     var ListGSRI = document.getElementsByClassName('gs_ri')
     for (var i_GSRI=0; i_GSRI < ListGSRI.length; i_GSRI++) {
+        // Get current year
         var d = new Date();
         var floatCurrentYear = parseFloat(d.getFullYear());
+        // Get publication year of item
         var GSRI = ListGSRI[i_GSRI]
         var Div_gs_a = GSRI.getElementsByTagName("div")[0]
         var Str_Year = Div_gs_a.textContent.match(/ [0-9][0-9][0-9][0-9] /g)[0]
         var floatPublichYear = parseFloat(Str_Year)
-
+        // Get citation number
         var arrayA = GSRI.getElementsByTagName("div")[2].getElementsByTagName("a")
         var aCitationNumer = arrayA[2];
         var floatCitationNumber= parseFloat(aCitationNumer.textContent.match(/[0-9]{1,}/)[0])
+        // Get citation per year
         var floatAverageCitation = floatCitationNumber / (floatCurrentYear + 1 - floatPublichYear)
 
+        // Add citation per year to the item
         var textAvgCitation = aCitationNumer.cloneNode()
         textAvgCitation.textContent = ", " + floatAverageCitation.toFixed(2).toString() + ' per year'
         textAvgCitation.style = ''
         arrayA[2].parentNode.insertBefore(textAvgCitation, aCitationNumer.nextSibling);
-        // arrayA.splice(3,0, divAvgCitation)
-        // console.log(textAvgCitation)
-        // console.log(arrayA[2].parentNode)
+
+        // Highlight year
+        console.log("year highlighted")
+        var strStyle = "background-color:powderblue;display: inline-block; border: 2px dashed black; padding: 0.03em 0.05em;"
+        if (floatPublichYear >= div_Input_Year.value) {
+            Div_gs_a.innerHTML = Div_gs_a.innerHTML.replace(Str_Year,'<b style=\"' + strStyle + '\">' + Str_Year + '</b>');
+        } else {
+            Div_gs_a.innerHTML = Div_gs_a.innerHTML.replace(strStyle,'');
+        }
+
+        // Highlight citation
+        if (floatCitationNumber >= div_Input_Cite.value){
+            aCitationNumer.style.backgroundColor = 'powderblue';
+            aCitationNumer.style.border = "2px dashed black";
+            // console.log(aCitationNumer.style.border);
+        } else {
+            aCitationNumer.style.backgroundColor = '';
+            aCitationNumer.style.border = "";
+        }
+
+        // Change alpha of unmatched item
+        if (floatPublichYear < div_Input_Year.value || floatCitationNumber < div_Input_Cite.value) {
+            GSRI.style.opacity = 0.5;
+        }
+        else {
+            GSRI.style.opacity = 1;
+        }
+
     }
     document.cookie = "Input_Year_Threshold=" + div_Input_Year.value
+    document.cookie = "Input_Cite_Value=" + div_Input_Cite.value
     return true
 }
+
+
 
 var gs_bdy_sb_in = document.getElementById("gs_bdy_sb_in")
 
@@ -109,22 +88,7 @@ gs_bdy_sb_in.appendChild(div_Control)
 
 var var_v_coord = 120
 var var_h_coord = window.innerWidth - 220
-// div_Control.style.position = "fixed";
-// div_Control.style.left = var_h_coord+"px"
-// div_Control.style.position = "absolute"
-// div_Control.style.top = "0"
-
-// div_Control.style.top = var_v_coord+"px"
-// div_Control.style.right = "20px"
-// div_Control.style.width = "200px";
-// div_Control.style.height = "120px";
-// div_Control.style.background = "rgba(255, 0, 0, 0.3)";
-// div_Control.style.color = "white";
-// div_Control.style.border = "solid"
-// div_Control.style.padding = "12px";
 div_Control.id = "id_coco_scholar_control"
-// div_Control.className = "input-group"
-// div_Control.innerHTML = "Control";
 div_Control.className = "gs_bdy_sb_sec"
 
 
@@ -181,11 +145,11 @@ div_Input_Year.className ="form-control"
 div_Input_Year.id = "id_coco_scholar_control_input_year"
 div_Input_Year.value=parseInt(getCookie("Input_Year_Threshold"));
 if (isNaN(div_Input_Year.value)) div_Input_Year.value = 2000;
+
 document.cookie = "Input_Year_Threshold=" + div_Input_Year.value
-// div_Input_Year.onchange = function(){Function_Highlight_Year()}
 div_Input_Year.style.width = "60px"
 
-window.addEventListener("resize", FunctionHighlightCitation);
+window.addEventListener("resize", FunctionModifyItems);
 
 var ButtonHighLight = document.createElement("button");
 ButtonHighLight.className = "btn"
@@ -193,9 +157,7 @@ ButtonHighLight.style.margin = "5px"
 console.log(ButtonHighLight.style.padding)
 ButtonHighLight.textContent = "highlight"
 ButtonHighLight.onclick = function(){
-FunctionHighlightCitation(); 
-Function_Highlight_Year();
-FunctionAddAverageCitation();
+    FunctionModifyItems()
 }
 
 div_Control.appendChild(div_Input_Cite_Title)
@@ -204,9 +166,7 @@ div_Control.appendChild(div_Input_Year_Title)
 div_Control.appendChild(div_Input_Year)
 div_Control.appendChild(ButtonHighLight)
 
-// document.getElementsByTagName("body")[0].appendChild(div_Control)
-
-
-FunctionHighlightCitation()
-Function_Highlight_Year()
-FunctionAddAverageCitation()
+FunctionModifyItems()
+// FunctionHighlightCitation()
+// Function_Highlight_Year()
+// FunctionAddAverageCitation()
